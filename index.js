5106f36c93ddd5bfacb837833907ad187f25a403
Sruthi <sruthivs64@gmail.com>
2023-07-05 03:31:57 +0530

    Fix restart logic, added styling

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ let finalWinner=" "
 let run=false;
 
 
+
 const rock=document.getElementById("rock")
-const paper=document.querySelector('paper')
-const scissors=document.querySelector('scissors')
+const paper=document.getElementById("paper")
+const scissors=document.getElementById("scissors")
+
+
+const buttons=document.querySelectorAll('input')
+
 
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3)
@@ -27,12 +32,24 @@ function restart(){
     const res=document.createElement("button")
     res.innerText="Restart"
     prompt.appendChild(res)
-    console.log(rock)
+    var styleButton = "align-self: center; background-color: rgba(245,244,240,255);font-size:40px;border-radius: 15px 225px 255px 15px 15px 255px 225px 15px;border-bottom-left-radius: 15px 255px;border-bottom-right-radius: 225px 15px;border-top-left-radius: 255px 15px;border-top-right-radius: 15px 225px;cursor: pointer;border-width: 2px;font-family: 'Caveat', cursive;font-family: 'Shadows Into Light', cursive;;line-height: 23px;padding: 12px 28px;";
+    res.setAttribute('style', styleButton);
+
     res.addEventListener('click', function(){
       window.location.reload(false);
     });
+    playerScore=0;
+    computerScore=0;
+    
     
 }
+function disableButtons(){
+  buttons.forEach(obj => {
+    obj.disabled = true
+})
+console.log("disabled")
+}
+
 function scorecard(descText, pScore, cScore, finaldesc){
   let computerPoints=document.getElementById('computerScore')
   let playerPoints=document.getElementById('playerScore')
@@ -47,13 +64,12 @@ function scorecard(descText, pScore, cScore, finaldesc){
   computerPoints.textContent=`Computer: ${cPoint}`
   playerPoints.textContent=`Player: ${pPoint}`
   if (res!=" "){
-    finalResult.textContent=res
-    
+    resultMessage.textContent=res
+
     if(!run){
     restart()
     }
-    };
-
+  }   
   } 
 
 function result(id){
@@ -78,14 +94,16 @@ function result(id){
         console.log(player)
         console.log(computer)
         
-        if(playerScore+computerScore==5){
+        if(playerScore===5||computerScore===5){
+        disableButtons()
         if(playerScore>computerScore){
-          finalWinner= "Congrats you won"
+          finalWinner= "Wohooo you won!!"
         }
         else {
-          finalWinner= "You lost!"
+          finalWinner= "Oops.....you lost!"
         }
       }
         scorecard(resultText, playerScore, computerScore, finalWinner)
+        
 
     }
